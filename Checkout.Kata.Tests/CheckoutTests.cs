ece7245f46bb26fac07c96aa6ce4ef9835195434
rkdev89 <richardkemp35@gmail.com>
2022-07-13 17:45:11 +0100

    Add project files.

---
+++ Checkout.Kata.Tests/CheckoutTests.cs
@@ -0,0 +1,17 @@
+namespace Checkout.Kata.Tests
+{
+    [TestFixture]
+    public class CheckoutTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
